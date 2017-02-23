3bac9b8a17efb3c7d2cb043eb67cdc4e374ed926
Andrew Harrison <andrew.harrison@nanthealth.com>
2017-02-23 19:47:35 +0000

    Adding Comparitor Model

---
+++ CacheProject/CacheProject/Model/ComparitorObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CacheProject.Model
+{
+    class ComparitorObject
+    {
+        public int VideoId;
+
+        public int CacheId;
+
+        public int EndpointId;
+
+        public int EffectiveTimeSaved;
+    }
+}
