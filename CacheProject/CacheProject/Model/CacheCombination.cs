6aa913d56fc1166f9f79422675a5539673d43425
Conor Fox <cfox@navinet.net>
2017-02-23 20:17:44 +0000

    cachecombo object

---
+++ CacheProject/CacheProject/Model/CacheCombination.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CacheProject.Model
+{
+    class CacheCombination
+    {
+        public List<int> VideoIds;
+
+        public int TotalEffectiveTimeSaved;
+    }
+}
